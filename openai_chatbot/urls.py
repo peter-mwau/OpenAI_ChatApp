bc92963233240790cfd520ef593b2e482cae67e4
Peter Kyale <kyalepeter2000@gmail.com>
2023-06-26 15:59:22 +0300

    Updated

--- openai_chatbot/urls.py
+++ openai_chatbot/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='chatpage'),
-    path('chat/', views.chat, name='chat'),
+    # path('', views.chat, name='chat'),
 ]
--- openai_chatbot/views.py
+++ openai_chatbot/views.py
@@ -3,45 +3,24 @@ from django.http import HttpResponse
 import openai
 
 def index(request):
-    openai.api_key = "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"
+    openai.api_key = "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"
     if request.method == "POST":
         sender = request.POST.get("message-input")
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"The following is a conversation with an AI assistant:\n\n"
-                   f"User: {sender}\n"
-                   f"Assistant:",
-            temperature=0.7,
-            max_tokens=50,
-            n=1,
-            stop=None
-        )
-        result = response.choices[0].text.strip()
-        context = {
-            'result': result
-        }
+        if sender:
+            completion = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo',
+                messages=[{"role": "user", "content": sender}]
+            )
+            result = completion['choices'][0]['message']['content']
+            print(result)
+            context = {
+                'result': result
+            }
+        else:
+            context = {}
     else:
         context = {}
 
     return render(request, 'index.html', context)
 
 
-def chat(request):
-    if request.method == "POST":
-        sender = request.POST.get("message-input")
-        # Perform AI processing and generate the AI response
-        # You can use the same OpenAI code here as in the index view
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"The following is a conversation with an AI assistant:\n\n"
-                   f"User: {sender}\n"
-                   f"Assistant:",
-            temperature=0.7,
-            max_tokens=50,
-            n=1,
-            stop=None
-        )
-        ai_response = response.choices[0].text.strip()
-        return HttpResponse(ai_response)
-    else:
-        return HttpResponse("Invalid request")

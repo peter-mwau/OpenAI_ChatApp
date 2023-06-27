5d81104d138de0cfa23341757a87d1366c5e000d
Peter Kyale <kyalepeter2000@gmail.com>
2023-06-27 21:11:55 +0300

    Updated views.py

--- openai_chatbot/views.py
+++ openai_chatbot/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import openai
+from gradio_client import Client
 
 def index(request):
-    openai.api_key = "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"
+    openai.api_key = "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"
     if request.method == "POST":
-        sender = request.POST.get("message-input")
-        # sender = "which is the largest ocean"
+        # sender = request.POST.get("message-input")
+
+        sender = "what is the fifa 23 in 5 words"
         messages = []
         messages.append({"role": "user", "content": sender})
         # sender = "Who is elon musk"
@@ -48,5 +50,25 @@ def index(request):
 #         return render(request, 'index.html', {'ai_response': ai_response})
 #     else:
 #         return render(request, 'index.html')
+
+
+
+# def index(request):
+#     if request.method == "POST":
+#         client = Client("https://a488d0b594b5d1d300.gradio.live/")
+#         result = client.predict(
+#             "what is javascript",
+#             api_name="/predict"
+#         )
+#         print(result)  # Check the value of result
+
+#         if result is not None:
+#             return render(request, 'index.html', {'ai_response': result})
+#         else:
+#             error_message = "An error occurred during prediction."
+#             return render(request, 'index.html', {'error_message': error_message})
+
+#     return render(request, 'index.html')
+
 
 
